1deb25b0908ecd5fa2492e2cbec1c7cbea469f74
mrsMistral <obalzak@list.ru>
2026-02-28 17:06:27 +0100

    Добавила постусловие с удалением курьера в тесты с неуспешной регистрацией

--- tests/test_create_courier.py
+++ tests/test_create_courier.py
@@ -19,34 +19,53 @@ class TestCreateCourier:
         assert response.status_code == 201
         assert response.json() == CREATE_COURIER_SUCCESS
 
-        # удаляем созданного курьера
+        # постусловие
         delete_courier(payload["login"], payload["password"])
 
 
-    def test_cannot_create_duplicate_courier(self, registered_courier):
-        login, password, first_name = registered_courier
-
+    def test_cannot_create_duplicate_courier(self):
         payload = {
-            "login": login,
-            "password": password,
-            "firstName": first_name
+            "login": generate_random_string(10),
+            "password": generate_random_string(10),
+            "firstName": generate_random_string(10)
         }
 
+        # создаём первого курьера
+        requests.post(CREATE_URL, json=payload)
+
+        # пробуем создать дубликат
         response = requests.post(CREATE_URL, json=payload)
 
         assert response.status_code == 409
         assert response.json()["message"] == LOGIN_ALREADY_EXISTS
 
+        # постусловие — удалить, даже если вдруг создался второй
+        delete_courier(payload["login"], payload["password"])
+
 
     def test_create_courier_without_login(self):
-        response = requests.post(CREATE_URL, json={"password": "1234"})
+        payload = {
+            "password": "1234"
+        }
+
+        response = requests.post(CREATE_URL, json=payload)
 
         assert response.status_code == 400
         assert response.json()["message"] == NOT_ENOUGH_DATA_CREATE
+
+        # защитное постусловие (если вдруг баг)
+        delete_courier(payload.get("login"), payload.get("password"))
 
 
     def test_create_courier_without_password(self):
-        response = requests.post(CREATE_URL, json={"login": "test"})
+        payload = {
+            "login": generate_random_string(10)
+        }
+
+        response = requests.post(CREATE_URL, json=payload)
 
         assert response.status_code == 400
         assert response.json()["message"] == NOT_ENOUGH_DATA_CREATE
+
+        # защитное постусловие
+        delete_courier(payload.get("login"), payload.get("password"))
