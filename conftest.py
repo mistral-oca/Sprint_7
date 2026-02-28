e7d4b3519d3840fe7cf125251dacf2c56febbcf5
mrsMistral <obalzak@list.ru>
2026-02-28 17:12:50 +0100

    Убрала тестовую проверку из conftest.py

--- conftest.py
+++ conftest.py
@@ -5,7 +5,6 @@ from helpers import register_new_courier_and_return_login_password, delete_couri
 @pytest.fixture
 def registered_courier():
     login_pass = register_new_courier_and_return_login_password()
-    assert login_pass != []
 
     login, password, first_name = login_pass
 
